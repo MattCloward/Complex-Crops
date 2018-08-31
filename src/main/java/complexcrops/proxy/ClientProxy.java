568a81b3ce6e44e1fdd6edc5d63699b96b7b4ac4
Icedice9 <mattcloward@gmail.com>
2018-08-30 18:40:34 -0600

    4.1.1 Fix

--- src/main/java/complexcrops/proxy/ClientProxy.java
+++ src/main/java/complexcrops/proxy/ClientProxy.java
@@ -1,7 +1,12 @@
 package complexcrops.proxy;
 
+import complexcrops.init.ModItems;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionUtils;
 import net.minecraftforge.client.model.ModelLoader;
 
 public class ClientProxy extends CommonProxy
@@ -9,5 +14,17 @@ public class ClientProxy extends CommonProxy
 	public void registerItemRenderer(Item item, int meta, String id)
 	{
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+	}
+	
+	public void init()
+	{		
+		//pickle colors registerer
+		Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new IItemColor()
+       		{
+            		public int colorMultiplier(ItemStack stack, int tintIndex)
+            		{
+                		return tintIndex == 0 ? PotionUtils.getColor(stack) : -1;
+            		}
+        	}, ModItems.PICKLE_POTION);
 	}
 }
